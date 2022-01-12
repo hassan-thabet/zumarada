cdc3097d1ac420daa9f4b6a703e7a92d548cc29e
unknown <eehabmsre@gmail.com>
2022-01-13 02:04:22 +0300

    eehab_cart

--- lib/ui/components/build_cart_section.dart
+++ lib/ui/components/build_cart_section.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:zumarada/bloc/home/home_tab/bloc.dart';
+import 'package:zumarada/constants/my_colors.dart';
+import 'package:zumarada/models/product.dart';
+import 'package:zumarada/ui/widgets/cart_widget.dart';
+import 'package:zumarada/ui/widgets/custom_button_widget.dart';
 import 'package:zumarada/ui/widgets/item_widget_in_cart.dart';
 
 class BulidCartSection extends StatelessWidget {
+  late Product product;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 5, itemBuilder: (context, index) => ItemWidgetInCart());
+    return Column(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Container(
+            child: ListView.builder(
+                itemCount: HomeTabBloc.get(context).products.length,
+                itemBuilder: (context, index) => ItemWidgetInCart(
+                      product: HomeTabBloc.get(context).products[index],
+                    )),
+          ),
+        ),
+        Expanded(flex: 1, child: CartWidget()),
+      ],
+    );
   }
 }
--- lib/ui/widgets/item_widget_in_cart.dart
+++ lib/ui/widgets/item_widget_in_cart.dart
@@ -1,90 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:zumarada/constants/my_colors.dart';
+import 'package:zumarada/models/product.dart';
 
 class ItemWidgetInCart extends StatelessWidget {
+  ItemWidgetInCart({required this.product});
+  late final Product product;
+  int a = 1;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 10, top: 10),
-          child: Column(
-            children: [
-              Row(
+    return Stack(
+      alignment: AlignmentDirectional.topEnd,
+      children: [
+        Container(
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10, top: 10),
+              child: Column(
                 children: [
-                  Image.network(
-                    'https://www.leylekkapida.com/Uploads/UrunResimleri/buyuk/bionike-defence-hydractive-mat-etkili--10-836.jpg',
-                    height: 60,
-                    width: 60,
-                  ),
-                  Container(
-                    width: 140,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'sstdkfjkl  ',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .copyWith(fontSize: 20, color: Colors.grey[900]),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'vnusdko thwa fdjidk ghoea; idkckajfi ',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .copyWith(fontSize: 15, color: Colors.grey[600]),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
+                  Row(
                     children: [
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Remove',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .copyWith(color: Colors.red, fontSize: 14),
-                          )),
-                      Row(
-                        children: [
-                          Container(
-                            height: 35,
-                            width: 90,
-                            decoration: BoxDecoration(
-                                color: Colors.grey[300],
-                                borderRadius: BorderRadius.circular(20)),
-                            child: Row(
-                              children: [
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(Icons.plus_one_outlined))
-                              ],
-                            ),
+                      FadeInImage.assetNetwork(
+                          height: 60,
+                          width: 60,
+                          fit: BoxFit.cover,
+                          placeholder: 'assets/images/loding.gif',
+                          image: product.image),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          width: 140,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product.title,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(
+                                        fontSize: 20, color: Colors.grey[900]),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                product.description,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .copyWith(
+                                        fontSize: 15, color: Colors.grey[600]),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          height: 20,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              InkWell(
+                                child: Icon(
+                                  Icons.add,
+                                  size: 14,
+                                ),
+                              ),
+                              Text(
+                                "$a",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              InkWell(
+                                child: Icon(
+                                  Icons.remove,
+                                  size: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('${product.price} EG',
+                          style: TextStyle(fontSize: 14, color: BUTTON_COLOR))
                     ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+        InkWell(
+            onTap: () {
+              print('object');
+            },
+            child: Icon(
+              Icons.delete,
+              color: Colors.red[300],
+              size: 20,
+            )),
+      ],
     );
   }
 }
